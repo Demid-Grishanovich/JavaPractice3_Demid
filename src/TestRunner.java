a3ed25c7c427c757982f99ac36755fc813367bbb
“Demid <email@address>
2023-05-04 15:31:36 +0300

    New push

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,34 +1,7 @@
 import task.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-/*public class TestRunner {    @Test
-public void TestConstructors(){
-    Purchase purchaseDefault = new Purchase();
-    assertEquals(0, purchaseDefault.getNumberOfPurchasedUnits());
-    assertEquals(0, purchaseDefault.getDiscountPercent());
-    assertNull( purchaseDefault.getWeekDay());
-    Purchase purchase = new Purchase(15, 30, WeekDay.Monday);
-    assertEquals("Iphone", purchase.getNAME());
-    assertEquals(70099, purchase.getPRICE());
-    assertEquals(15, purchase.getNumberOfPurchasedUnits());
-    assertEquals(30, purchase.getDiscountPercent());
-    assertEquals(WeekDay.Monday, purchase.getWeekDay());
-}
-    @Test
-    public void testGetCost(){
-        Purchase purchase1 = new Purchase(6, 20, WeekDay.Tuesday);
-        assertEquals(336500, purchase1.getCost());
-        Purchase purchase2 = new Purchase(10, 10, WeekDay.Monday);
-        assertEquals(630900, purchase2.getCost());
-        Purchase purchase3 = new Purchase(3, 0, WeekDay.Sunday);
-        assertEquals(210300, purchase3.getCost());
-    }
-    @Test
-    public void testConversionToEuro(){
-        Purchase purchase2 = new Purchase();
-        assertEquals(purchase2.convertToEuro(120053), "1200.53");
-    }
-}*/
+
 
 public class TestRunner {
     @Test
